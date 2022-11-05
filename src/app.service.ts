e9e4ae23191dfcf0452b6f56ce567157f122ad09
admin <admin@CryptoFairy.local>
2022-11-05 12:26:06 +0200

    added connection to local node + local server support

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,11 +4,12 @@ import Web3 from 'web3';
 @Injectable()
 export class AppService {
   async connect(): Promise<string> {
-    const web3 = new Web3('http://localhost:8545');
+    const web3 = new Web3('http://master:8545');
     const balance = await web3.utils.fromWei(
       await web3.eth.getBalance('0x8da7245ED352ea6A278657Fd47Fe1E31c177c51C'),
       'ether'
    ); 
+   console.log(balance);
     return balance;
   }
 }
